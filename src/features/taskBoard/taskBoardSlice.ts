920531000ad009eb6c4fdc1c6fc673ce081cddda
ThanhTai <jinlee8298@gmail.com>
2021-08-31 22:42:53 +0700

    add default state for access website the first time

--- src/features/taskBoard/taskBoardSlice.ts
+++ src/features/taskBoard/taskBoardSlice.ts
@@ -1,12 +1,12 @@
 import { createSlice, EntityId } from "@reduxjs/toolkit";
-import { COLOR_LIST } from "common/constants";
 import taskReducer, { taskAdapter } from "./store/taskReducer";
 import sectionReducer, { sectionAdapter } from "./store/sectionReducer";
 import labelReducer, { labelAdapter } from "./store/labelReducer";
 import projectReducer, { projectAdapter } from "./store/projectReducer";
 import commentReducer, { commentAdapter } from "./store/commentReducer";
+import defaultState from "./store/defaultStore";
 
-const defaultState = {
+const defaultStateTemplate = {
   tasks: taskAdapter.getInitialState<{
     draggingInfo: {
       draggingTaskId: EntityId;
@@ -23,28 +23,16 @@ const defaultState = {
       placeholderHeight: string;
     } | null;
   }>({ draggingInfo: null }),
-  projects: projectAdapter.getInitialState({
-    ids: ["2021"],
-    entities: {
-      "2021": {
-        id: "2021",
-        name: "Welcome",
-        color: COLOR_LIST[0].color,
-        sectionIds: [],
-        filterOptions: { showCompletedTask: false },
-      },
-    },
-  }),
+  projects: projectAdapter.getInitialState(),
   comments: commentAdapter.getInitialState(),
   labels: labelAdapter.getInitialState(),
 };
 
-const peristedState: typeof defaultState = JSON.parse(
+const peristedState: typeof defaultStateTemplate = JSON.parse(
   localStorage.getItem("store") || "{}"
 )?.taskBoard;
 
 export const initialState = peristedState || defaultState;
-
 export type TaskBoardStore = typeof initialState;
 
 export const taskBoardSlice = createSlice({

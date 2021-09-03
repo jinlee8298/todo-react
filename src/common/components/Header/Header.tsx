69bb5371e6141acb3d2b8c218e846fe8839d6fca
ThanhTai <jinlee8298@gmail.com>
2021-09-03 15:55:24 +0700

    modify navbar into modal

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -4,41 +4,51 @@ import StyledHeader from "./Header.style";
 import { FC, ReactElement, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import { useHistory } from "react-router-dom";
+import Button from "../Button/Button";
 
 type HeaderProps = {
   navContent?: ReactElement;
 };
 
 const Header: FC<HeaderProps> = ({ navContent }) => {
-  const [showNavBar, setShowNavBar] = useState(true);
+  const [showNavBar, setShowNavBar] = useState(false);
   const history = useHistory();
-  const onToggleNavBar = () => {
-    setShowNavBar((v) => !v);
+  const closeNavBar = () => {
+    setShowNavBar(false);
   };
+
+  const openNavBar = () => {
+    setShowNavBar(true);
+  };
+
   const onToHomePage = () => {
     history.push("/");
   };
 
   return (
     <StyledHeader className={showNavBar ? "show-nav" : ""}>
-      <NavBar>{navContent}</NavBar>
+      <NavBar onClose={closeNavBar} isShown={showNavBar}>
+        {navContent}
+      </NavBar>
       <div className="left-group">
-        <button
+        <Button
           aria-label="Open side navigation tab"
           title="Open side navigation tab"
-          onClick={onToggleNavBar}
-          className="toggle-nav"
-        >
-          <FontAwesomeIcon icon={faBars} fixedWidth />
-        </button>
-        <button
+          onClick={openNavBar}
+          icon={faBars}
+          variant="white"
+          alternative="reverse"
+          size="sx"
+        />
+        <Button
           aria-label="Go to homepage"
           title="Go to homepage"
           onClick={onToHomePage}
-          className="toggle-nav"
-        >
-          <FontAwesomeIcon icon={faHome} fixedWidth />
-        </button>
+          icon={faHome}
+          variant="white"
+          alternative="reverse"
+          size="sx"
+        />
       </div>
     </StyledHeader>
   );
